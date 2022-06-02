549ec6c3a5c055b72830854217bd5e0ab71dc10b
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-06-02 14:55:29 -0400

    created classes and query methods

---
+++ LINQAnimals/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LINQAnimals
+{
+    public class Owner
+    {
+        public Owner()
+        {
+        }
+
+        public string Name { get; set; }
+        public int OwnerID { get; set; }
+    }
+}
--- LINQAnimals/Program.cs
+++ LINQAnimals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQAnimals
 {
@@ -6,7 +7,58 @@ namespace LINQAnimals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryStringArray();
+
+            QueryIntArray();
+
+            Console.ReadKey();
+        }
+
+        static void QueryStringArray()
+        {
+            string[] dogs = { "K 9", "Brian Griffin", "Scooby Doo", "Old Yeller", "Rin Tin Tin", "Benji", "Charlie B. Barkin", "Lassie", "Snoopy", "Maya" };
+
+            var dogSpaces = from dog in dogs
+                            where dog.Contains(" ")
+                            orderby dog descending
+                            select dog;
+
+            foreach (var i in dogSpaces)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+
+        static int[] QueryIntArray()
+        {
+            int[] nums = { 5, 10, 15, 20, 25, 30, 35 };
+
+            var greaterThan20 = from num in nums
+                                where num > 20
+                                orderby num
+                                select num;
+
+            foreach (var i in greaterThan20)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Get Type : {greaterThan20.GetType()}");
+
+            var listGreaterThan20 = greaterThan20.ToList<int>();
+            var arrayGreaterThan20 = greaterThan20.ToArray();
+
+            nums[0] = 40;
+
+            foreach (var i in greaterThan20)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            return arrayGreaterThan20;
         }
     }
 }
